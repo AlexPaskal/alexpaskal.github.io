cf889949c9708c809984077e2f7014032848212f
Alex Paskal <alexpaskal@i.ua>
2016-07-10 23:59:47 +0300

    App version 2.0

--- index/js/app.js
+++ index/js/app.js
@@ -1,16 +1,14 @@
 ﻿var app = angular.module('AppModule', ['firebase']);
-
-
 app.controller('AppCtrl', function ($scope, $firebaseObject) {
     var ref = new Firebase('https://mypoj-c6cf8.firebaseio.com')
     ref.on("value", function (snapshot) {
-        $scope.data = snapshot.val().text;
+        $('#span1').text(snapshot.val().text);
     }, function (errorObject) {
         console.log("The read failed: " + errorObject.code);
     });
     $scope.FormSubmit = function () {
         var curtxt = $scope.edit1;
         ref.update({ text: curtxt });
-        $scope.data = curtxt;
+        $('#span1').text(curtxt);
     }
 });
