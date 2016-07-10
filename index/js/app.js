510f6ff191d7d4df1b8e7a83e223c4d10eb99783
Alex Paskal <alexpaskal@i.ua>
2016-07-10 18:59:57 +0300

    version3

--- index/js/app.js
+++ index/js/app.js
@@ -1,3 +1,9 @@
 ﻿angular.module('AppModule', []).controller('AppCtrl', function ($scope) {
+    var text = "";
     $scope.he = 'AngularJS';
+    $scope.FormSubmit = function () {
+        text += $scope.edit1;
+        text += " ";
+        $scope.text = text;
+    }
 });
