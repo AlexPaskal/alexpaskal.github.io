da1c98e8385fef8f8cd98a00f517f43def3562a0
Alex Paskal <alexpaskal@i.ua>
2016-07-13 20:56:45 +0300

    Auth (not working)

--- index/js/app.js
+++ index/js/app.js
@@ -1,14 +1,26 @@
-﻿var app = angular.module('AppModule', ['firebase']);
-app.controller('AppCtrl', function ($scope, $firebaseObject) {
-    var ref = new Firebase('https://mypoj-c6cf8.firebaseio.com')
-    ref.on("value", function (snapshot) {
+﻿//'use strict';
+
+var app = angular.module('AppModule', ['firebase']);
+app.controller('AppCtrl', ['$scope', '$firebaseSimpleLogin', function ($scope, $firebaseSimpleLogin) {
+    var ref = new Firebase('https://mypoj-c6cf8.firebaseio.com');
+    var loginObj = $firebaseSimpleLogin(ref);
+    loginObj.$login('password', {
+        email: 'paskal-os@i.ua',
+        password: 'password'
+    }).then(function (user) {
+        console.log('Authentication successful');
+    }, function (error) {
+        console.log('Authentication failed');
+    });
+
+    /*ref.on('value', function (snapshot) {
         $('#span1').text(snapshot.val().text);
     }, function (errorObject) {
-        console.log("The read failed: " + errorObject.code);
-    });
+        console.log('The read failed: ' + errorObject.code);
+    });*/
     $scope.FormSubmit = function () {
         var curtxt = $scope.edit1;
         ref.update({ text: curtxt });
         $('#span1').text(curtxt);
     }
-});
+}]);
