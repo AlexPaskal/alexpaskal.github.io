6e7faa693e1a1237c4d6793deeb1425d529577c8
Alex Paskal <alexpaskal@i.ua>
2016-07-17 22:16:31 +0300

    Not working

--- index/js/app.js
+++ index/js/app.js
@@ -1,6 +1,7 @@
-﻿//'use strict';
+﻿//'use strict';**
 
 var app = angular.module('AppModule', ['firebase']);
+
 app.controller('AppCtrl', ['$scope', '$firebaseSimpleLogin', function ($scope, $firebaseSimpleLogin) {
     var ref = new Firebase('https://mypoj-c6cf8.firebaseio.com');
     var loginObj = $firebaseSimpleLogin(ref);
@@ -13,7 +14,19 @@ app.controller('AppCtrl', ['$scope', '$firebaseSimpleLogin', function ($scope, $
         console.log('Authentication failed');
     });
 
-    /*ref.on('value', function (snapshot) {
+
+    /*ref.authWithPassword({
+        email: 'paskal-os@i.ua',
+        password: 'password'
+    }, function (error, data) {
+        if (error) {
+            console.log('error');
+        } else {
+            colsole.log('good');
+        }
+    });
+
+    ref.on('value', function (snapshot) {
         $('#span1').text(snapshot.val().text);
     }, function (errorObject) {
         console.log('The read failed: ' + errorObject.code);
@@ -24,3 +37,33 @@ app.controller('AppCtrl', ['$scope', '$firebaseSimpleLogin', function ($scope, $
         $('#span1').text(curtxt);
     }
 }]);
+
+//app.controller('AppCtrl', function ($scope, $firebaseSimpleLogin) {
+    //var ref = new Firebase('https://mypoj-c6cf8.firebaseio.com');
+    //var loginObj = $firebaseSimpleLogin(ref);
+    //loginObj.$login('password', {
+    //    email: 'paskal-os@i.ua',
+    //    password: 'password'
+    //}).then(function (user) {
+    //    console.log('Authentication successful');
+    //}, function (error) {
+    //    console.log('Authentication failed');
+    //});
+    //ref.authWithPassword({
+    //    email: 'bobtony@firebase.com',
+    //    password: 'correcthorsebatterystaple'
+    //}, function (error, data) {
+    //
+    //});
+    //
+    /*ref.on('value', function (snapshot) {
+        $('#span1').text(snapshot.val().text);
+    }, function (errorObject) {
+        console.log('The read failed: ' + errorObject.code);
+    });
+    $scope.FormSubmit = function () {
+        var curtxt = $scope.edit1;
+        ref.update({ text: curtxt });
+        $('#span1').text(curtxt);
+    }
+});*/
